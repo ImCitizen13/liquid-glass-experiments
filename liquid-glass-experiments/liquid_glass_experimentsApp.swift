9a2484ffdb1c540d9bc2997885d628c00e140721
ImCitizen13 <meltohamy@protonmail.com>
2026-05-08 11:20:17 +0400

    Initial Commit

---
+++ liquid-glass-experiments/liquid_glass_experimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  liquid_glass_experimentsApp.swift
+//  liquid-glass-experiments
+//
+//  Created by MOHAMED ELABASIRI on 08/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct liquid_glass_experimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ liquid-glass-experimentsTests/liquid_glass_experimentsTests.swift
@@ -0,0 +1,19 @@
+//
+//  liquid_glass_experimentsTests.swift
+//  liquid-glass-experimentsTests
+//
+//  Created by MOHAMED ELABASIRI on 08/05/2026.
+//
+
+import Testing
+@testable import liquid_glass_experiments
+
+struct liquid_glass_experimentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
